6f12fd95d9f53009738db4747eb1fb8de3cc4049
Ali Hammoud <alloochi.hammoud@gmail.com>
2021-04-03 20:54:30 +0300

    Introduction to redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import MainComponent  from './components/MainComponent';
 
+const store = ConfigureStore();
 
 function App() {
-  
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <MainComponent />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <MainComponent />
+        </div>
+      </BrowserRouter>
+    </Provider>  
+    
     
   );
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,6 @@
 
 
 import MenuComponent  from './MenuComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
 import { useState } from 'react'
 import DishDetailComponent from './DishDetailComponent';
 import HeaderComponent from './HeaderComponent';
@@ -12,25 +8,31 @@ import FooterComponent from './FooterComponent';
 import HomeComponent from './HomeComponent';
 import ContactComponent from './ContactComponent';
 import AboutComponent from './AboutComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const MainComponent = () => {
-  const [dishes,setDishes] = useState(DISHES);
-  const [comments, setComments] = useState(COMMENTS);
-  const [leaders, setLeaders] = useState(LEADERS);
-  const [promotions, setPromotions] = useState(PROMOTIONS)
+const mapStateToProps = (state) => {
+    return {
+      dishes: state.dishes,
+      comments: state.comments,
+      promotions: state.promotions,
+      leaders: state.leaders
+    }
+}
+
+const MainComponent = (props) => {
 
   const HomePage = () => {
-      return ( <HomeComponent dish={dishes.filter(dish => dish.featured)[0]} 
-          promotion={promotions.filter(promo => promo.featured)[0]}
-          leader={leaders.filter(ld => ld.featured)[0]}/> 
+      return ( <HomeComponent dish={props.dishes.filter(dish => dish.featured)[0]} 
+          promotion={props.promotions.filter(promo => promo.featured)[0]}
+          leader={props.leaders.filter(ld => ld.featured)[0]}/> 
        );
   }
 
   const DishWithID = ({match}) => {
     return(
-        <DishDetailComponent dish={dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]}
-            comments={comments.filter(comment => comment.dishId === parseInt(match.params.dishId, 10))}
+        <DishDetailComponent dish={props.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]}
+            comments={props.comments.filter(comment => comment.dishId === parseInt(match.params.dishId, 10))}
         />
     );
   }
@@ -41,9 +43,9 @@ const MainComponent = () => {
       <HeaderComponent />
       <Switch>
           <Route path='/home' component={HomePage} />
-          <Route path='/aboutus' component={() => <AboutComponent leaders={leaders}/>} />
+          <Route path='/aboutus' component={() => <AboutComponent leaders={props.leaders}/>} />
           <Route exact path='/menu' 
-          component={() => <MenuComponent dishes={dishes} />} /> 
+          component={() => <MenuComponent dishes={props.dishes} />} /> 
           {/* We can pass props to the component*/}
           <Route path='/menu/:dishId' component={DishWithID} />
           <Route exact path="/contactus" component={ContactComponent}/>
@@ -54,4 +56,4 @@ const MainComponent = () => {
   );
 }
 
-export default MainComponent;
+export default withRouter(connect(mapStateToProps)(MainComponent));
